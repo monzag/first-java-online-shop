8754a7c71798372fd856d2499fa39e2cbc8ccaa5
monzag <zaganiacz.m@gmail.com>
2017-09-01 15:08:18 +0200

    Fix ProductCategory

--- frompythontojava/onlineshop/part1/ProductCategory.java
+++ frompythontojava/onlineshop/part1/ProductCategory.java
@@ -1,11 +1,17 @@
 package frompythontojava.onlineshop.part1;
 
 public class ProductCategory {
-
+    private static Integer id = 1;
     private String name;
+    private Integer ID;
+
+    public ProductCategory() {
+
+    }
     
     public ProductCategory(String name) {
         this.name = name;
+        this.ID = id++;
     }
 
     public String getName() {
