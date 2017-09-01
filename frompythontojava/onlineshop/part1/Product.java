9b8bef626c200a1366fec1a890ade3ac3e4d7502
monzag <zaganiacz.m@gmail.com>
2017-09-01 15:07:52 +0200

    Refactor Product class

--- frompythontojava/onlineshop/part1/Product.java
+++ frompythontojava/onlineshop/part1/Product.java
@@ -3,33 +3,45 @@ import java.util.ArrayList;
 
 public class Product {
 
+    private static Integer id = 1;
+    private static ArrayList<Product> productList = new ArrayList<>();
+
     private String name;
     private Float defaultPrice;
     private ProductCategory productCategory;
     private Integer ID;
+    
+    public Product() {
+    }
 
     public Product(String name, Float defaultPrice, ProductCategory productCategory) {
         this.name = name;
         this.defaultPrice = defaultPrice;
         this.productCategory = productCategory;
-        this.ID = getID();
-
-    }
-
-    public static Integer getID() {
-        Integer ID = 1;
-        return ID;
+        this.ID = id++;
     }
 
     public String toString() {
-        return String.format("ID:%d,name:%s,defaultprice:%f,productCategory:%s", ID, name, defaultPrice, productCategory.getName());
+        String info;
+        if (productCategory instanceof FeaturedProductCategory) {
+            info = String.format("FeaturedProductCategory,ID:%d,name:%s,defaultprice:%f,productCategory:%s", ID, name, defaultPrice, productCategory.getName());
+        } else {
+            info = String.format("ID:%d,name:%s,defaultprice:%f,productCategory:%s", ID, name, defaultPrice, productCategory.getName());
+        }
+        return info;
     }
 
-    // public ArrayList getAllProducts() {
-    //     ;
-    // }
+    public static ArrayList getAllProducts() {
+        return productList;
+    }
 
-    // public ArrayList getAllProductsBy(ProductCategory productCategory) {
-    //     ;
-    // }
+    public static ArrayList getAllProductsBy(ProductCategory productCategory) {
+        ArrayList<Product> sameCategory = new ArrayList<>();
+        for (Product product : productList) {
+            if (product.productCategory.equals(productCategory)) {
+                sameCategory.add(product);
+            }
+        }
+        return sameCategory;
+    }
 }
