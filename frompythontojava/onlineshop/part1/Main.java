2a5eedf37e8d7a50be0e2aa96e2daef8dfc2bab3
monzag <zaganiacz.m@gmail.com>
2017-09-03 23:59:00 +0200

    Add main in part1

--- frompythontojava/onlineshop/part1/Main.java
+++ frompythontojava/onlineshop/part1/Main.java
@@ -1,8 +1,30 @@
 package frompythontojava.onlineshop.part1;
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+    
+        ProductCategory category = new ProductCategory("books");
+        System.out.println(category.getName());
+
+        Product product = new Product("Winds of winter", 25.5f, category);
+        System.out.println(product);
+        System.out.println(product.toString());
+        System.out.println(product.getAllProducts());
+        System.out.println(product.getAllProductsBy(category));
+
+        Date date = new Date(2017, 10, 10);
+        FeaturedProductCategory featuredCategory = new FeaturedProductCategory("SuperBooks", date);
+        System.out.println(featuredCategory);
+        System.out.println(featuredCategory.getExspirationDate());
+
+        Basket basket = new Basket();
+        System.out.println(basket.getProductList());
+        basket.addProduct(product);
+        System.out.println(basket.getProductList());
+        // basket.removeProduct(product);
+        // System.out.println(basket.productList);
+
+        }
 }
