5cadd10d3d2e71afc7bf9fcad8ef4a851d5dd47c
monzag <zaganiacz.m@gmail.com>
2017-09-04 02:58:43 +0200

    Add main

--- frompythontojava/onlineshop/part2/Main.java
+++ frompythontojava/onlineshop/part2/Main.java
@@ -3,6 +3,19 @@ package frompythontojava.onlineshop.part2;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	
+    Order order = new Order();
+    System.out.println(order.getStatus());
+    order.checkout();
+    System.out.println(order.getStatus());
+    order.pay();
+    System.out.println(order.getStatus());
+
+    CheckoutProcess checkout = new CheckoutProcess();
+    checkout.process(order);
+
+    PaymentProcess payment = new PaymentProcess();
+    payment.process(order);
+
     }
 }
